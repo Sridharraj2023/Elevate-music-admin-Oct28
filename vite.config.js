9e9eac931066d3a37dc48666e118003f7942bbd6
Sridhar R <sridharraj0110@gmail.com>
2025-10-23 22:25:21 +0530

    New origin and port added

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
+    port: 5173,
+    host: true, // Allow external connections
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        target: process.env.VITE_API_URL || 'http://172.232.30.46:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'), // Keep /api in the request
       },
